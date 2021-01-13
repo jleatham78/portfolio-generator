0f12234135bcaec5d758929d01d2f765b06a0817
jleatham78 <janeleleatham01@gmail.com>
2021-01-12 22:31:46 -0700

    changed stuff

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+const generatePage = () => "Name: Jane, Github; janehub";
+console.log(generagePage());
+
+// const generatePage = (userName, githubName) => {
+//     return
+//         Name: ${userName}
+//         github; ${githubName}
+//         ;
+// };
+// }
