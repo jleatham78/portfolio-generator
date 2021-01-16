996045715b2878e3f5d05c1135ae7ac5985d124c
jleatham78 <janeleleatham01@gmail.com>
2021-01-15 17:51:27 -0700

    added feature project

--- app.js
+++ app.js
@@ -182,11 +182,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
+        //const pageHTML = generatePage(mockData);
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('index.html', pageHTML, err => {
+        fs.writeFile('./index.html', pageHTML, err => {
             if (err) throw new Error(err);
 
-        //     console.log("Page created! Check out index.html in this directory to see it!");
+        console.log("Page created! Check out index.html in this directory to see it!");
          }); 
     });
